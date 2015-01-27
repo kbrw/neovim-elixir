b8245148f3aca345b7d904333779fd063f807cbb
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-01-27 03:39:41 +0100

    starts to log only after neovim starts log

--- lib/logger.ex
+++ lib/logger.ex
@@ -1,13 +1,21 @@
 defmodule NVim.Logger do
   use GenEvent
 
-  def handle_event({level,_leader,{Logger,msg,_ts,_md}},state) do
-    clean_msg = msg |> to_string |> String.split("\n") |> hd |> String.replace("\"","\\\"")
-    NVim.vim_command ~s/echo "#{level}: #{clean_msg}"/
-    {:ok,state}
-  catch _, _ -> {:ok,state}
+  def handle_event({level,_leader,{Logger,msg,_ts,_md}},:activated) do
+    NVim.vim_command ~s/echo "#{level}: #{clean_msg msg}"/
+    {:ok,:activated}
+  end
+  def handle_event({_,_,{Logger,["Application ","neovim"," started at "|_],_,_}},_), do: {:ok,:activated}
+  def handle_event(_,state), do: {:ok,state}
+
+  defp clean_msg(msg) do
+    msg 
+    |> to_string 
+    |> String.split("\n")
+    |> hd
+    |> String.replace("\"","\\\"")
   end
 
-  def handle_call({:configure,_opts},state), do:
+  def handle_call({:configure,_opts},state), do: 
     {:ok,:ok,state}
 end
