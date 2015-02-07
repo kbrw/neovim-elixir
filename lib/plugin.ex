87045135c2bb40e3bffe0add4bc617e9cef601ac
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-02-07 03:26:47 +0100

    change plug macro API

--- lib/plugin.ex
+++ lib/plugin.ex
@@ -22,40 +22,38 @@ defmodule NVim.Plugin do
   end
 
   def eval_specs(params), do:
-    Enum.filter(params,fn {k,_}-> not k in [:async,:range,:count,:bang,:register,:pattern,:buffer,:bar,:group,:nested] end)
-  def command_values_specs(params), do:
-    Enum.filter(params,fn {k,_}-> k in [:range,:count,:bang,:register] end)
-  def command_other_specs(params), do:
-    Enum.filter(params,fn {k,_}-> k in [:buffer,:bar] end)
+    Enum.filter(params,fn {k,_}-> k in [:eval] end)
   def autocmd_specs(params), do:
     Enum.filter(params,fn {k,_}-> k in [:group,:nested] end)
+  def command_other_specs(params), do:
+    Enum.filter(params,fn {k,_}-> k in [:buffer,:bar] end)
+  def command_values_specs(params), do:
+    Enum.filter(params,fn {k,_}-> k in [:range,:count,:bang,:register] end)
+  def command_args_specs(params) do
+    Enum.filter(params,fn {k,_}-> k in [:range,:count,:bang,:register,:eval] end)
+  end
 
   def wrap_reply({status,reply,state},_,_) when status in [:ok,:error], do:
     {:reply,{status,reply},state}
   def wrap_reply(other,initstate,_), do:
     {:reply,{:ok,other},initstate}
 
-  def params_to_eval(params) do
-    map = params
-    |> Enum.map(fn {k,v}->"'#{k}': #{v}" end)
-    |> Enum.join(", ")
-    "{#{map}}"
-  end
+  defp list_or_noarg([]), do: []
+  defp list_or_noarg(args), do: [args]
 
   def sig_to_sigwhen({:when,_,[{name,_,params},guard]}), do: {name,params,guard}
   def sig_to_sigwhen({name,_,params}), do: {name,params,true}
 
   defmacro deffunc(signature, funcparams \\ [], [do: body]) do
     {name,params,guard} = sig_to_sigwhen(signature)
-    [state|params] = Enum.reverse(params)
     eval_specs = eval_specs(funcparams)
-    {eval_args,params} = if eval_specs == [] do 
-      {[],params}
-    else
-      [eval|params] = params
-      {[eval],params}
-    end
+    [state|params] = Enum.reverse(params)
+
+    {eval_args,params} = Enum.split(params,length(eval_specs))
+    eval_args = list_or_noarg(Enum.reverse(eval_args))
+
     nargs_args = [Enum.reverse(params)]
+
     call_args = Enum.concat([nargs_args,eval_args])
     name = Mix.Utils.camelize("#{name}")
     quote do
@@ -64,7 +62,8 @@ defmodule NVim.Plugin do
                   type: "function", 
                   name: unquote(name),
                   sync: unquote(if(funcparams[:async], do: 0,else: 1)),
-                  opts: %{unquote_splicing(if eval_specs == [],do: [], else: [eval: params_to_eval(eval_specs)])}
+                  opts: %{unquote_splicing(if eval_specs == [],do: [], 
+                            else: [eval: "[#{eval_specs|>Dict.values|>Enum.join(",")}]"])}
                 }))
       def handle_call({:function,unquote(name),unquote(call_args)},var!(nvim_from),unquote(state)=initialstate) when unquote(guard) do
         wrap_reply(unquote(body),initialstate,unquote(funcparams[:async] in [nil,false]))
@@ -72,37 +71,33 @@ defmodule NVim.Plugin do
     end
   end
 
-  def wrap_spec_value(_,true), do: ""
-  def wrap_spec_value(:range,:default_all), do: "%"
-  def wrap_spec_value(:range,:default_line), do: ""
-  def wrap_spec_value(_,value), do: value
+  defp wrap_spec_value(_,true), do: ""
+  defp wrap_spec_value(:range,:default_all), do: "%"
+  defp wrap_spec_value(:range,:default_line), do: ""
+  defp wrap_spec_value(_,value), do: value
 
   defmacro defcommand(signature, funcparams \\ [], [do: body]) do
     {name,params,guard} = sig_to_sigwhen(signature)
-    [state|params] = Enum.reverse(params)
-    eval_specs = eval_specs(funcparams)
-    {eval_args,params} = if eval_specs == [] do 
-      {[],params}
-    else
-      [eval|params] = params
-      {[eval],params}
-    end
     values_specs = command_values_specs(funcparams)
-    {special_args,params} = if values_specs == [] do
-      {[],params}
-    else
-      {special,params} = Enum.split(params,length(values_specs))
-      special_dict = Enum.zip(Dict.keys(values_specs),Enum.reverse(special))
-      arg_order = [range: 0,count: 1,bang: 2,register: 3]
-      special = special_dict |> Enum.sort_by(fn {k,_}->arg_order[k] end) |> Dict.values
-      {special,params}
-    end
-    {nargs_args,defaults} = if params == [] do {[],[]} else
-      with_defaults = Enum.reverse(params)
-      without_defaults = Enum.map(with_defaults, fn {:\\,_,[e,_]}->e; e->e end)
-      defaults = Enum.map(with_defaults,fn {:\\,_,[_,default]}->default; _->nil end)
-      {[without_defaults],defaults}
-    end
+    eval_specs = eval_specs(funcparams)
+    [state|params] = Enum.reverse(params)
+
+    eval_indexes = for {{:eval,_},i}<-Enum.with_index(funcparams), do: i
+    {special_eval_args,params} = Enum.split(params,length(values_specs) + length(eval_specs))
+    special_eval_args = Enum.reverse(special_eval_args)
+    eval_args = Enum.map(eval_indexes,&Enum.at(special_eval_args,&1))
+    eval_args = list_or_noarg(eval_args)
+
+    special_args = Enum.reduce(eval_indexes,special_eval_args,&List.delete_at(&2,&1))
+    special_dict = Enum.zip(Dict.keys(values_specs),special_args)
+    nvim_arg_order = [range: 0,count: 1,bang: 2,register: 3]
+    special_args = special_dict |> Enum.sort_by(fn {k,_}->nvim_arg_order[k] end) |> Dict.values
+
+    with_defaults = Enum.reverse(params)
+    without_defaults = Enum.map(with_defaults, fn {:\\,_,[e,_]}->e; e->e end)
+    defaults = Enum.map(with_defaults,fn {:\\,_,[_,default]}->default; _->nil end)
+    nargs_args = list_or_noarg(without_defaults)
+
     call_args = Enum.concat([nargs_args,special_args,eval_args])
     default_call_args = Enum.concat([[quote do: _],special_args,eval_args])
     name = Mix.Utils.camelize("#{name}")
@@ -115,7 +110,7 @@ defmodule NVim.Plugin do
                   opts: %{unquote_splicing(
                       if(nargs_args == [], do: [], else: [nargs: "*"]) ++
                       Enum.map(values_specs++command_other_specs(funcparams),fn {k,v}->{k,wrap_spec_value(k,v)} end) ++
-                      if(eval_specs == [], do: [], else: [eval: params_to_eval(eval_specs)])
+                      if(eval_specs == [], do: [], else: [eval: "[#{eval_specs|>Dict.values|>Enum.join(",")}]"])
                     )}
                 }))
       def handle_call({:command,unquote(name),unquote(call_args)},var!(nvim_from),unquote(state)=initialstate)  when unquote(guard)  do
@@ -132,9 +127,10 @@ defmodule NVim.Plugin do
 
   defmacro defautocmd(signature, funcparams \\ [], [do: body]) do
     {name,params,guard} = sig_to_sigwhen(signature)
-    [state|params] = Enum.reverse(params)
     eval_specs = eval_specs(funcparams)
-    call_args = if eval_specs == [] do [] else params end
+    [state|params] = Enum.reverse(params)
+
+    call_args = list_or_noarg(Enum.reverse(params))
     name = Mix.Utils.camelize("#{name}")
     pattern = funcparams[:pattern] || "*"
     quote do
@@ -146,7 +142,7 @@ defmodule NVim.Plugin do
                   opts: %{unquote_splicing(
                       [pattern: pattern]++
                       Enum.map(autocmd_specs(funcparams),fn {k,v}->{k,wrap_spec_value(k,v)} end) ++
-                      if(eval_specs == [],do: [], else: [eval: params_to_eval(eval_specs)])
+                      if(eval_specs == [],do: [], else: [eval: "[#{eval_specs|>Dict.values|>Enum.join(",")}]"])
                     )}
                 }))
       def handle_call({:autocmd,{unquote(name),unquote(pattern)},unquote(call_args)},var!(nvim_from),unquote(state)=initialstate) when unquote(guard) do
--- test/neovim_test.exs
+++ test/neovim_test.exs
@@ -1,41 +1,41 @@
 defmodule TestPlugin do
   use NVim.Plugin
 
-  defcommand cmd_n1(arg1,arg2 \\ "toto",bang,count,eval,_), bang: true, count: true, toto: "xx" do
-    {arg1,arg2,bang,count,eval}
+  defcommand cmd_n1(arg1,arg2 \\ "toto",bang,eval1,count,eval2,_), bang: true, eval: "xx", count: true, eval: "xx" do
+    {arg1,arg2,bang,eval1,count,eval2}
   end
 
-  deffunc fun1(arg1,arg2,eval,_), toto: "xx" do
-    {arg1,arg2,eval}
+  deffunc fun1(arg1,arg2,eval1,eval2,_), eval: "xx", eval: "xx" do
+    {arg1,arg2,eval1,eval2}
   end
 
-  defautocmd auto_cmd1(eval,_), pattern: "*.ex", toto: "xx" do
-    eval
+  defautocmd auto_cmd1(eval1,eval2,_), pattern: "*.ex", eval: "xx", eval: "xx" do
+    {eval1,eval2}
   end
 end
 defmodule NeovimTest do
   use ExUnit.Case
 
   test "defcommand param arrangement" do
-    assert {:reply,{:ok,{:n1,:n2,1,34,%{"toto"=>3}}},:state} = 
+    assert {:reply,{:ok,{:n1,:n2,1,"toto",34,3}},:state} = 
       TestPlugin.handle_call({:command,"CmdN1",
-          [[:n1,:n2],34,1,%{"toto"=>3}]},nil,:state)
+          [[:n1,:n2],34,1,["toto",3]]},nil,:state)
   end
 
   test "defcommand default params" do
-    assert {:reply,{:ok,{nil,"toto",1,34,%{}}},:state} = 
-      TestPlugin.handle_call({:command,"CmdN1",[[],34,1,%{}]},nil,:state)
+    assert {:reply,{:ok,{nil,"toto",1,"toto",34,3}},:state} = 
+      TestPlugin.handle_call({:command,"CmdN1",[[],34,1,["toto",3]]},nil,:state)
   end
 
   test "deffunc param arrangement" do
-    assert {:reply,{:ok,{:n1,:n2,%{"toto"=>3}}},:state} = 
+    assert {:reply,{:ok,{:n1,:n2,"toto",3}},:state} = 
       TestPlugin.handle_call({:function,"Fun1",
-          [[:n1,:n2],%{"toto"=>3}]},nil,:state)
+          [[:n1,:n2],["toto",3]]},nil,:state)
   end
 
   test "defautocmd param arrangement" do
-    assert {:reply,{:ok,%{"toto"=>3}},:state} = 
+    assert {:reply,{:ok,{"toto",3}},:state} = 
       TestPlugin.handle_call({:autocmd,{"AutoCmd1","*.ex"},
-          [%{"toto"=>3}]},nil,:state)
+          [["toto",3]]},nil,:state)
   end
 end
